2af2762816bed8067d66cfae11da154ab2af965f
cqb13 <cqb13.main@gmail.com>
2025-02-24 09:46:59 -0500

    get starting and ending words for word ladder file

--- src/word_ladder.rs
+++ src/word_ladder.rs
@@ -1,3 +1,5 @@
+use crate::utils::extract_cstring_with_offset;
+
 #[derive(Debug)]
 pub struct WordLadder {
     pub starting_word: String,
@@ -6,4 +8,29 @@ pub struct WordLadder {
     pub ending_word_def: String,
     pub steps: u8,
     pub step_hints: Vec<String>,
+}
+
+impl WordLadder {
+    pub fn load(bytes: &[u8]) -> Result<WordLadder, String> {
+        let (starting_word, offset) = extract_cstring_with_offset(bytes, 0);
+        let (starting_word_def, offset) = extract_cstring_with_offset(bytes, offset);
+        let (ending_word, offset) = extract_cstring_with_offset(bytes, offset);
+        let (ending_word_def, offset) = extract_cstring_with_offset(bytes, offset);
+
+        // TODO: get the steps and add 1 to offset at next parse
+        //
+
+        let steps: u8 = 0;
+
+        let mut step_hints: Vec<String> = Vec::new();
+
+        Ok(WordLadder {
+            starting_word,
+            starting_word_def,
+            ending_word,
+            ending_word_def,
+            steps,
+            step_hints,
+        })
+    }
 }
